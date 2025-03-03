e2e60c925e59125d7cf0c07798c268654c4e3781
jafarlow <jacobafarlow@gmail.com>
2025-03-03 14:23:58 -0500

    Report generator

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from stats import get_word_count
 from stats import get_character_count
+from stats import sorted_dictionary
 
-def main(book):
-  text = get_book_text(book)
-  get_word_count(text)
-  get_character_count(text)
+def main():
+  path = "./books/frankenstein.txt"
+  text = get_book_text(path)
+  count = get_character_count(text)
+  letters = sorted_dictionary(count)
+  report(text, path, letters)
+  # get_word_count(text)
 
 def get_book_text(path):
   with open(path) as f:
     return f.read()
     
-main("./books/frankenstein.txt")
+def report(text_input, path_to_text, characters):
+  print("============ BOOKBOT ============")
+  print(f"Analyzing book found at {path_to_text}...")
+  print("----------- Word Count ----------")
+  count = get_word_count(text_input)
+  print(f"Found {count} total words")
+  print("--------- Character Count -------")
+  for k, v in characters.items():
+    print(f"{k}: {v}")
+  print("============= END ===============")
+
+main()
--- stats.py
+++ stats.py
@@ -15,3 +15,9 @@ def get_character_count(text):
       char_count[lowercased] = 1
   # print(char_count)
   return char_count
+
+def sorted_dictionary(dict_input):
+  filtered_dict = {k: v for k, v in dict_input.items() if k.isalpha()}
+  sorted_dict = dict(sorted(filtered_dict.items(), key=lambda item: item[1], reverse=True))
+  # print(sorted_dict)
+  return sorted_dict
