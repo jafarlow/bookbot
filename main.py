e8109b13fcbed6318ac4a95836b3db104694038f
jafarlow <jacobafarlow@gmail.com>
2025-03-03 13:00:42 -0500

    Getting ramped up

---
+++ main.py
@@ -0,0 +1,10 @@
+def main(book):
+  get_book_text(book)
+
+def get_book_text(file_to_read):
+  with open(file_to_read) as f:
+    contents = f.read().split()
+    count = len(contents)
+    print(f"{count} words found in the document")
+
+main("./books/frankenstein.txt")
